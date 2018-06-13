879119b3c958ad34f64d83b32a5a5ffe88c30cff
Cristi Ghinea <christian.ghinea@gmail.com>
2018-06-13 17:24:31 +0200

    Project setup: Set MVVM design pattern. Set project.

---
+++ Model/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  Xapo
+//
+//  Created by Cristian Florin Ghinea on 13/06/2018.
+//  Copyright © 2018 Cristian Florin Ghinea. All rights reserved.
+//
+
+import Foundation
+
+final class Project {
+    
+}
